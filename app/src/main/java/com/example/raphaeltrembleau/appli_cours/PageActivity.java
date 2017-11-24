456b95b8a618fb14407aa593829b24ce3da5c23e
raphaeltrembleau <trembleau.raphael@gmail.com>
2017-11-24 14:19:09 +0100

    bouton annuler Page Activity

--- app/src/main/java/com/example/raphaeltrembleau/appli_cours/PageActivity.java
+++ app/src/main/java/com/example/raphaeltrembleau/appli_cours/PageActivity.java
@@ -1,13 +1,24 @@
 package com.example.raphaeltrembleau.appli_cours;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PageActivity extends AppCompatActivity {
+public class PageActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
+        button = (Button) findViewById(R.id.buttonCancel);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
     }
 }

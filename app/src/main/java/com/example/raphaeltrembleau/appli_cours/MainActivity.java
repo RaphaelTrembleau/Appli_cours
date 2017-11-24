c156badf22cac5251249b378767fd99f5643c5e1
raphaeltrembleau <trembleau.raphael@gmail.com>
2017-11-24 13:29:24 +0100

    setOnClick

--- app/src/main/java/com/example/raphaeltrembleau/appli_cours/MainActivity.java
+++ app/src/main/java/com/example/raphaeltrembleau/appli_cours/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.raphaeltrembleau.appli_cours;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, PageActivity.class);
+        startActivity(intent);
     }
 }
